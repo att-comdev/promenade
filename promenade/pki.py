b5fd4bd8398664bd2408213dc45f9c84decd3b89
Mark Burnett <mark.m.burnett@gmail.com>
2017-06-10 21:23:15 -0500

    refactor key gen/distribution

--- promenade/pki.py
+++ promenade/pki.py
@@ -10,22 +10,37 @@ __all__ = ['generate_keys']
 LOG = logging.getLogger(__name__)
 
 
+DISTRIBUTION_MAP = {
+    'sa': [
+        'apiserver',
+    ],
+    'sa-key': [
+        'controller-manager',
+    ],
+}
+
+
 def generate_keys(target):
     with tempfile.TemporaryDirectory() as tmp:
-        private_key = os.path.join(tmp, 'sa.key')
-        public_key = os.path.join(tmp, 'sa.pub')
-        key_target_dir = os.path.join(target, 'etc/kubernetes/controller-manager/pki')
-        pub_target_dir = os.path.join(target, 'etc/kubernetes/apiserver/pki')
-        key_target = os.path.join(key_target_dir, 'sa.key')
-        pub_target = os.path.join(pub_target_dir, 'sa.pub')
+        _make_sa_keypair(tmp)
 
+        _distribute_files(tmp, target)
+
+
+def _make_sa_keypair(output_dir):
+        private_key = os.path.join(output_dir, 'sa-key.pem')
+        public_key = os.path.join(output_dir, 'sa.pem')
         subprocess.run(['/usr/bin/openssl', 'genrsa',
                         '-out', private_key], check=True)
         subprocess.run(['/usr/bin/openssl', 'rsa', '-pubout',
                         '-in', private_key,
                         '-out', public_key], check=True)
 
-        os.makedirs(key_target_dir, exist_ok=True)
-        os.makedirs(pub_target_dir, exist_ok=True)
-        shutil.move(private_key, key_target)
-        shutil.move(public_key, pub_target)
+
+def _distribute_files(src, dest):
+    for filename, destinations in DISTRIBUTION_MAP.items():
+        src_path = os.path.join(src, filename + '.pem')
+        for destination in destinations:
+            dest_dir = os.path.join(dest, 'etc/kubernetes/%s/pki' % destination)
+            os.makedirs(dest_dir, exist_ok=True)
+            shutil.copy(src_path, dest_dir)
