bec81aa2f2c1eb127dc6f93e2da3b5237f8a38f7
Aric Renzo <ar7520@att.com>
2017-06-28 14:29:44 -0400

    Adding support for logging rendered file hasing

--- promenade/renderer.py
+++ promenade/renderer.py
@@ -2,6 +2,7 @@ from . import logging
 import jinja2
 import os
 import pkg_resources
+import hashlib
 
 __all__ = ['Renderer']
 
@@ -28,6 +29,8 @@ class Renderer:
                 source_path = os.path.join(root, source_filename)
                 self.render_template_file(path=source_path,
                                           root=source_root)
+                self.generate_sha_hashes(path=source_path,
+                                          root=source_root)
 
     def render_template_file(self, *, path, root):
         base_path = os.path.relpath(path, root)
@@ -47,6 +50,32 @@ class Renderer:
             f.write(rendered_data)
 
         LOG.info('Installed "%s"', os.path.join('/', base_path))
+
+    def generate_sha_hashes(self, *, path, root):
+        base_path = os.path.relpath(path, root)
+        target_path = os.path.join(self.target_dir, base_path)
+        log_path = "var/log/promenade/"
+        LOG.info('VARIABLE REVEAL: self.target_dir is:"%s" and base_path is: "%s" and target_path is "%s"', self.target_dir, base_path, target_path)
+        target_log_path = os.path.join(self.target_dir, log_path)
+        log_file = os.path.join(target_log_path, "modified_file_check.txt")
+        try:
+            LOG.debug('Creating "%s"', target_log_path)
+            os.mkdir(target_log_path)
+        except OSError:
+            LOG.debug('Skipping creation of "%s" because it already exists', target_log_path)
+
+        hash_sums = {}
+        hash = hashlib.sha256(open(path,'rb').read()).hexdigest()
+        hash_sums[target_path] = hash
+        #os.chdir(log_path)
+        LOG.info('HASHING:  "%s" into "%s"', hash_sums, target_path)
+        LOG.info('Writing:  "%s"', log_file)
+        os.chdir(target_log_path)
+        file_out = open(log_file, "a")
+        for key,value in hash_sums.items():
+            output = key + ": " + value
+            file_out.write(output + "\n")
+        file_out.close()
 
 
 def _ensure_path(path):
