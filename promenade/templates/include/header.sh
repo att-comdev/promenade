9ba7987f8b0db918a1128ad0f0d792c1bd7c4abd
Mark Burnett <mark.m.burnett@gmail.com>
2017-09-08 16:07:51 -0500

    WIP: Migrate to self hosted using charts
    This change includes several interconnected features:
    
    * Migration to Deckhand-based configuration.  This is integrated here,
      because new configuration data were needed, so it would have been
      wasted effort to either implement it in the old format or to update
      the old configuration data to Dechkand format.
    * Failing faster with stronger validation.  Migration to Deckhand
      configuration was a good opportunity to add schema validation, which
      is a requirement in the near term anyway.  Additionally, rendering
      all templates up front adds an additional layer of "fail-fast".
    * Separation of certificate generation and configuration assembly into
      different commands.  Combined with Deckhand substitution, this creates
      a much clearer distinction between Promenade configuration and
      deployable secrets.
    * Migration of components to charts.  This is a key step that will
      enable support for dynamic node management.  Additionally, this paves
      the way for significant configurability in component deployment.
    * Version of kubelet is configurable.
    * Restructuring templates to be more intuitive.  Many of the templates
      require changes or deletion due to the migration to charts.
    * Installation of pre-configured useful tools on hosts, including calicoctl.
    * DNS is now provided by coredns, which is highly configurable.
    
    This change is currently a WIP, because it needs to following work to
    bring it up to parity with master:
    
    * Documentation describing configuration and getting started needs to be
      updated.
    
    Change-Id: I9f2d8da6346f4308be5083a54764ce6035a2e10c

---
+++ promenade/templates/include/header.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Ensure the script is running as root.
+#
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root." 1>&2
+   exit 1
+fi
+
+function log {
+    echo $(date) $* 1>&2
+}
