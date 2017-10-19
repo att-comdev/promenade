40c21c0123b235ab00f664a3b6b74c73b25404da
Mark Burnett <mark.m.burnett@gmail.com>
2017-10-19 09:02:54 -0500

    WIP: Add virsh-based dev and testing environment
    This is intended to address difficulties in setting up a development
    environment, and provide a locally-runnable gate script.
    
    Change-Id: I4900d34437f9fe735f580ab91b38a6bb5424481e

---
+++ tools/virsh/gate/build-image.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$(realpath $(dirname $0))
+WORKSPACE=${WORKSPACE:-$(realpath $SCRIPT_DIR/../../..)}
+
+IMAGE_PROMENADE=${IMAGE_PROMENADE:-quay.io/attcomdev/promenade:latest}
+
+
+sudo docker build -t $IMAGE_PROMENADE $WORKSPACE
