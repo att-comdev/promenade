aa447bc6de5270a0c9a3f162c4d5b12917a7c3ce
Mark Burnett <mark.m.burnett@gmail.com>
2017-10-19 09:02:54 -0500

    WIP: Add virsh-based dev and testing environment
    This is intended to address difficulties in setting up a development
    environment, and provide a locally-runnable gate script.
    
    Change-Id: I4900d34437f9fe735f580ab91b38a6bb5424481e

---
+++ tools/virsh/gate/create-vms.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+VMS=${@:-n0 n1 n2 n3}
+
+SCRIPT_DIR=$(realpath $(dirname $0))
+WORKSPACE=${WORKSPACE:-$(realpath $SCRIPT_DIR/../../..)}
+
+BASE_IMAGE_URL=https://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img
+
+if ! virsh vol-key --pool default --vol promenade-base.img; then
+    pushd $(mktemp -d)
+    curl -L -o base.img $BASE_IMAGE_URL
+    virsh vol-create-as \
+        --pool default \
+        --name promenade-base.img \
+        --format qcow2 \
+        --capacity 68719476736 \
+        --prealloc-metadata
+
+    virsh vol-upload \
+        --vol promenade-base.img \
+        --file base.img \
+        --pool default
+    popd
+fi
+
+$WORKSPACE/tools/virsh/destroy.sh
+$WORKSPACE/tools/virsh/create.sh
+
+for vm in $VMS; do
+    $WORKSPACE/tools/virsh/rsync.sh $WORKSPACE/tools/virsh/tmpgate/scripts/* $vm:/root/promenade &
+done
+
+wait
