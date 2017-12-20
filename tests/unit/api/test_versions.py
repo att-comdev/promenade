28439cea73c54f682cfb7c3c64741c4f6c21f0f2
Mark Burnett <mark.m.burnett@gmail.com>
2017-12-20 09:29:49 -0600

    Minor testing-related cleanup
    * Move docs dependencies out of test-requirements.txt
    * Eliminate python version specific test (py35)
    * Re-order default envlist to shorten feedback cycle
    * Minor whitespace changes
    * Simplify versions unit test
    * Add /.cache to .gitignore
    * Include tests directory in basic format requirements, but not flake8
    * Fix sample noauth-api-paste.ini config
    
    Change-Id: I440f5c2d988098287f9914449f7103735f832192

--- tests/unit/api/test_versions.py
+++ tests/unit/api/test_versions.py
@@ -10,9 +10,8 @@ from promenade.promenade import promenade
 def client():
     return testing.TestClient(promenade)
 
+
 def test_get_versions(client):
     response = client.simulate_get('/versions')
     assert response.status == falcon.HTTP_200
-    body = response.content.decode('utf-8')
-    assert '"path": "/api/v1.0"' in body
-    assert '"status": "stable"' in body
+    assert response.json == {'v1.0': {'path': '/api/v1.0', 'status': 'stable'}}
