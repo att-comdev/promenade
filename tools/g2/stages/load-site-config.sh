1c6cc3cf5493560e96c91354993100fa7a3eea83
Mark Burnett <mark.m.burnett@gmail.com>
2017-12-20 13:08:03 -0600

    WIP: Add deckhand design_ref support
    WIP due to:
    * Cannot put CertificateAuthority/CertificateAuthorityKey and
      Public/PrivateKey documents into Deckhand.
    * Certificate/CertificateKey documents returned by Deckhand have
      modified (invalid) data sections.
    
    * Add ability to fetch design from Deckhand
    * Add functional testing for deckhand fetch integration
    
    Change-Id: Ice25a27b340e68a8ab38a23021cd91e032ca537b

---
+++ tools/g2/stages/load-site-config.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eu
+
+source "${GATE_UTILS}"
+
+while getopts "v:" opt; do
+    case "${opt}" in
+        v)
+            VIA=${OPTARG}
+            ;;
+        *)
+            echo "Unknown option"
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ $# -gt 0 ]; then
+    echo "Unknown arguments specified: ${*}"
+    exit 1
+fi
+
+TOKEN=$(os_ks_get_token "${VIA}")
+
+DECKHAND_URL=http://deckhand-int.ucp.svc.cluster.local:9000/api/v1.0/buckets/prom/documents
+
+rsync_cmd "${TEMP_DIR}/nginx/promenade.yaml" "${VIA}:/root/promenade/promenade.yaml"
+ssh_cmd "${VIA}" curl -v \
+    --fail \
+    -H "X-Auth-Token: ${TOKEN}" \
+    -H "Content-Type: application/x-yaml" \
+    -T "/root/promenade/promenade.yaml" \
+    "${DECKHAND_URL}"
