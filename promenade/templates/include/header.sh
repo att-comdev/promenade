673cc535710ee149dfb355c2633e56ab83179642
Mark Burnett <mark.m.burnett@gmail.com>
2018-01-24 11:32:41 -0600

    Fix: regression shebang line of genesis.sh/join.sh
    This also moves the common hostname validation logic into header.sh
    
    Change-Id: I6b74496d741ceedf3ba0122549bacb2813dd0001

--- promenade/templates/include/header.sh
+++ promenade/templates/include/header.sh
@@ -4,6 +4,13 @@ set -e
 
 {% include "utils.sh" with context %}
 
+# Validate the hostname is as expected
+#
+if [ "$(hostname)" != "{{ config.get_first('Genesis:hostname', 'KubernetesNode:hostname') }}" ]; then
+   echo "The node hostname must match the Kubernetes node name" 1>&2
+   exit 1
+fi
+
 # Ensure the script is running as root.
 #
 if [ "$(id -u)" != "0" ]; then
