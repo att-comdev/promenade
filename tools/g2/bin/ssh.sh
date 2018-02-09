09520ae3d27993878fca65577e5742efaf289b38
Mark Burnett <mark.m.burnett@gmail.com>
2018-02-09 13:46:13 -0600

    Update complete example to reflect recent changes
    * Adds retries to parts of the teardown script.
    * Caches tar_urls from Promenade config.
    
    Change-Id: I009eff2479cf486d03bececc975aba6c08b9a8f6

--- tools/g2/bin/ssh.sh
+++ tools/g2/bin/ssh.sh
@@ -5,6 +5,7 @@ set -e
 SCRIPT_DIR=$(realpath $(dirname $0))
 WORKSPACE=$(realpath ${SCRIPT_DIR}/../../..)
 GATE_UTILS=${WORKSPACE}/tools/g2/lib/all.sh
+TEMP_DIR=${TEMP_DIR:-$(mktemp -d)}
 
 source ${GATE_UTILS}
 
--- tools/stop_gate.sh
+++ tools/stop_gate.sh
@@ -5,6 +5,7 @@ set -e
 SCRIPT_DIR=$(realpath "$(dirname "${0}")")
 WORKSPACE=$(realpath "${SCRIPT_DIR}/..")
 GATE_UTILS=${WORKSPACE}/tools/g2/lib/all.sh
+TEMP_DIR=${TEMP_DIR:-$(mktemp -d)}
 
 export GATE_UTILS
 export WORKSPACE
