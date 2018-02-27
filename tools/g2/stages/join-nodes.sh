3ebe0e62a560e39825d1686a8097cb3a2db6731b
Samantha Blanco <spblanco.1@gmail.com>
2018-02-27 18:30:02 -0500

    Add Promenade client
    Adds Promenade client and session management for consumption by
    other UCP projects and corresponding CLI commands. Also adds client
    documentation.
    
    Change-Id: Ie00b0a8a628788257c979019aecb2c95c8330e4a

--- tools/g2/stages/join-nodes.sh
+++ tools/g2/stages/join-nodes.sh
@@ -45,51 +45,33 @@ if [ $# -gt 0 ]; then
     exit 1
 fi
 
-SCRIPT_DIR="${TEMP_DIR}/curled-scripts"
-BASE_PROM_URL="http://promenade-api.ucp.svc.cluster.local"
-
 echo Etcd Clusters: "${ETCD_CLUSTERS[@]}"
 echo Labels: "${LABELS[@]}"
 echo Nodes: "${NODES[@]}"
 
-render_curl_url() {
-    NAME=${1}
-    shift
+render_labels() {
     LABELS=(${@})
 
     LABEL_PARAMS=
     for label in "${LABELS[@]}"; do
-        LABEL_PARAMS+="&labels.dynamic=${label}"
+        LABEL_PARAMS+="-dl ${label} "
     done
 
-    BASE_URL="${BASE_PROM_URL}/api/v1.0/join-scripts"
-    if [[ ${USE_DECKHAND} == 1 ]]; then
-        DESIGN_REF="design_ref=deckhand%2Bhttp://deckhand-int.ucp.svc.cluster.local:9000/api/v1.0/revisions/${DECKHAND_REVISION}/rendered-documents"
-    else
-        DESIGN_REF="design_ref=${NGINX_URL}/promenade.yaml"
-    fi
-    HOST_PARAMS="hostname=${NAME}&ip=$(config_vm_ip "${NAME}")"
-
-    echo "${BASE_URL}?${DESIGN_REF}&${HOST_PARAMS}${LABEL_PARAMS}"
+    echo "${LABEL_PARAMS}"
 }
 
-render_validate_url() {
-    BASE_URL="${BASE_PROM_URL}/api/v1.0/validatedesign"
-    if [[ ${USE_DECKHAND} == 1 ]]; then
-        HREF="href=deckhand%2Bhttp://deckhand-int.ucp.svc.cluster.local:9000/api/v1.0/revisions/${DECKHAND_REVISION}/rendered-documents"
-    else
-        HREF="href=${NGINX_URL}/promenade.yaml"
-    fi
+if [[ ${USE_DECKHAND} == 1 ]]; then
+    REF_URL="deckhand%2Bhttp://deckhand-int.ucp.svc.cluster.local:9000/api/v1.0/revisions/${DECKHAND_REVISION}/rendered-documents"
+else
+    REF_URL="${NGINX_URL}/promenade.yaml"
+fi
 
-    echo "${BASE_URL}?${HREF}"
-}
 
 mkdir -p "${SCRIPT_DIR}"
 
 for NAME in "${NODES[@]}"; do
     log Building join script for node "${NAME}"
 
-    CURL_ARGS=("--fail" "--max-time" "300" "--retry" "16" "--retry-delay" "15")
     if [[ $GET_KEYSTONE_TOKEN == 1 ]]; then
         TOKEN="$(os_ks_get_token "${VIA}")"
         if [[ -z $TOKEN ]]; then
@@ -97,13 +79,13 @@ for NAME in "${NODES[@]}"; do
             exit 1
         fi
         log "Got keystone token: ${TOKEN}"
-        CURL_ARGS+=("-H" "X-Auth-Token: ${TOKEN}")
+        TOKEN="--token ${TOKEN}"
     fi
 
     log "Checking Promenade API health"
     MAX_HEALTH_ATTEMPTS=6
     for attempt in $(seq ${MAX_HEALTH_ATTEMPTS}); do
-        if ssh_cmd "${VIA}" curl -v "${CURL_ARGS[@]}" "${BASE_PROM_URL}/api/v1.0/health"; then
+        if promenade_api_health "${NAME}" "${TOKEN}"; then
             log "Promenade API healthy"
             break
         elif [[ $attempt == "${MAX_HEALTH_ATTEMPTS}" ]]; then
@@ -114,19 +96,18 @@ for NAME in "${NODES[@]}"; do
     done
 
     log "Validating documents"
-    ssh_cmd "${VIA}" curl -v "${CURL_ARGS[@]}" -X POST "$(render_validate_url)"
+    promenade_api_validatedesign "${NAME}" "${REF_URL}" "${TOKEN}"
 
-    JOIN_CURL_URL="$(render_curl_url "${NAME}" "${LABELS[@]}")"
-    log "Fetching join script via: ${JOIN_CURL_URL}"
-    ssh_cmd "${VIA}" curl "${CURL_ARGS[@]}" \
-        "${JOIN_CURL_URL}" > "${SCRIPT_DIR}/join-${NAME}.sh"
+    log "Fetching join script"
+    JOIN_LABELS="$(render_labels)"
+    ARGS="--hostname "${NAME}" --ip "$(config_vm_ip "${NAME}")" --design-ref "${REF_URL}" --dl "${JOIN_LABELS}" "${TOKEN}""
+    promenade_api_join_scripts "$ARGS"
 
-    chmod 755 "${SCRIPT_DIR}/join-${NAME}.sh"
     log "Join script received"
 
     log Joining node "${NAME}"
-    rsync_cmd "${SCRIPT_DIR}/join-${NAME}.sh" "${NAME}:/root/promenade/"
     ssh_cmd "${NAME}" "/root/promenade/join-${NAME}.sh" 2>&1 | tee -a "${LOG_FILE}"
+    exit
 done
 
 sleep 10
