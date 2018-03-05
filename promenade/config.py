b92ffdaba398989e09e8fbe75a39f3af89da88e8
Mark Burnett <mark.m.burnett@gmail.com>
2018-03-05 14:10:03 -0600

    Allow adding new definitions to PKICatalog
    * Detect and re-use existing Certs/Keys
    * Negative functional test for join with missing cert
    * Positive functional test to generate cert after initial construction
    * Extract some promenade test code into tools/g2/lib/promenade.sh
    * Add timestamps to tar'd up files
    
    Change-Id: Ib717785fc2c8f6cd1db1970ecdf1f5184ed40e92

--- promenade/config.py
+++ promenade/config.py
@@ -90,7 +90,7 @@ class Configuration:
                 'No document found matching kind=%s schema=%s name=%s' %
                 (kind, schema, name))
 
-    def iterate(self, *, kind=None, schema=None, labels=None):
+    def iterate(self, *, kind=None, schema=None, labels=None, name=None):
         if kind is not None:
             if schema is not None:
                 raise AssertionError(
@@ -98,8 +98,13 @@ class Configuration:
             schema = 'promenade/%s/v1' % kind
 
         for document in self.documents:
-            if _matches_filter(document, schema=schema, labels=labels):
+            if _matches_filter(
+                    document, schema=schema, labels=labels, name=name):
                 yield document
+
+    def find(self, *args, **kwargs):
+        for doc in self.iterate(*args, **kwargs):
+            return doc
 
     def extract_genesis_config(self):
         LOG.debug('Extracting genesis config.')
@@ -175,7 +180,7 @@ class Configuration:
                              ['/apiserver', '--apiserver-count=2', '--v=5'])
 
 
-def _matches_filter(document, *, schema, labels):
+def _matches_filter(document, *, schema, labels, name):
     matches = True
     if schema is not None and not document.get('schema',
                                                '').startswith(schema):
@@ -189,6 +194,10 @@ def _matches_filter(document, *, schema, labels):
             else:
                 if document_labels[key] != value:
                     matches = False
+
+    if name is not None:
+        if _mg(document, 'name') != name:
+            matches = False
 
     return matches
 
