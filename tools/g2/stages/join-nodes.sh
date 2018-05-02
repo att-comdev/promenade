a35de8cb184f16079cfce7f8a0ca73e3d6f889f3
Samantha Blanco <spblanco.1@gmail.com>
2018-05-02 17:47:35 -0400

    Add Promenade client
    Adds Promenade client and session management for consumption by
    other UCP projects and corresponding CLI commands. Also adds client
    documentation.
    
    Change-Id: Ie00b0a8a628788257c979019aecb2c95c8330e4a

--- tools/g2/stages/join-nodes.sh
+++ tools/g2/stages/join-nodes.sh
@@ -11,8 +11,10 @@ declare -a NODES
 GET_KEYSTONE_TOKEN=0
 USE_DECKHAND=0
 DECKHAND_REVISION=''
+OS_AUTH_URL=""
+PROM_URL=""
 
-while getopts "d:e:l:n:tv:" opt; do
+while getopts "d:e:l:n:a:u:p:r:s:o:m:tv:" opt; do
     case "${opt}" in
         e)
             ETCD_CLUSTERS+=("${OPTARG}")
@@ -33,6 +35,27 @@ while getopts "d:e:l:n:tv:" opt; do
         v)
             VIA=${OPTARG}
             ;;
+        a)
+            OS_AUTH_URL=${OPTARG}
+            ;;
+        u)
+            OS_USERNAME=${OPTARG}
+            ;;
+        p)
+            OS_PASSWORD=${OPTARG}
+            ;;
+        r)
+            OS_PROJECT_NAME=${OPTARG}
+            ;;
+        s)
+            OS_USER_DOMAIN_NAME=${OPTARG}
+            ;;
+        o)
+            OS_PROJECT_DOMAIN_NAME=${OPTARG}
+            ;;
+        m)
+            PROM_URL=${OPTARG}
+            ;;
         *)
             echo "Unknown option"
             exit 1
@@ -47,17 +70,34 @@ if [ $# -gt 0 ]; then
 fi
 
 SCRIPT_DIR="${TEMP_DIR}/curled-scripts"
-
 echo Etcd Clusters: "${ETCD_CLUSTERS[@]}"
 echo Labels: "${LABELS[@]}"
 echo Nodes: "${NODES[@]}"
+
+render_labels() {
+    LABELS=(${@})
+
+    LABEL_PARAMS=
+    for label in "${LABELS[@]}"; do
+        LABEL_PARAMS+="-dl ${label} "
+    done
+
+    echo "${LABEL_PARAMS}"
+}
+
+if [[ ${USE_DECKHAND} == 1 ]]; then
+    REF_URL="deckhand%2Bhttp://deckhand-int.ucp.svc.cluster.local:9000/api/v1.0/revisions/${DECKHAND_REVISION}/rendered-documents"
+else
+    REF_URL="${NGINX_URL}/promenade.yaml"
+fi
 
 mkdir -p "${SCRIPT_DIR}"
 
 for NAME in "${NODES[@]}"; do
     log Building join script for node "${NAME}"
 
-    CURL_ARGS=("--fail" "--max-time" "300" "--retry" "16" "--retry-delay" "15")
+    ARGS=""
+
     if [[ $GET_KEYSTONE_TOKEN == 1 ]]; then
         TOKEN="$(os_ks_get_token "${VIA}")"
         if [[ -z $TOKEN ]]; then
@@ -66,25 +106,39 @@ for NAME in "${NODES[@]}"; do
         fi
         TOKEN_HASH=$(echo -n "${TOKEN}" | md5sum)
         log "Got keystone token, token md5sum: ${TOKEN_HASH}"
-        CURL_ARGS+=("-H" "X-Auth-Token: ${TOKEN}")
+        ARGS="--token ${TOKEN}"
+    elif [[ $OS_AUTH_URL ]]; then
+        ARGS="--os_auth_url ${OS_AUTH_URL} --os_username ${OS_USERNAME} --os_user_domain_name ${OS_USER_DOMAIN_NAME} --os_password ${OS_PASSWORD} --os_project_name ${OS_PROJECT_NAME} --os_project_domain_name ${OS_PROJECT_DOMAIN_NAME}"
     fi
 
-    promenade_health_check "${VIA}"
+    ARGS="${ARGS} --url ${PROM_URL}"
+
+    log "Checking Promenade API health"
+    MAX_HEALTH_ATTEMPTS=6
+    for attempt in $(seq ${MAX_HEALTH_ATTEMPTS}); do
+        if promenade_api_health "${VIA}" "${ARGS}"; then
+            log "Promenade API healthy"
+            break
+        elif [[ $attempt == "${MAX_HEALTH_ATTEMPTS}" ]]; then
+            log "Promenade health check failed, max retries (${MAX_HEALTH_ATTEMPTS}) exceeded."
+            exit 1
+        fi
+        sleep 10
+    done
 
     log "Validating documents"
-    ssh_cmd "${VIA}" curl -v "${CURL_ARGS[@]}" -X POST -H "Content-Type: application/json" -d "$(promenade_render_validate_body "${USE_DECKHAND}" "${DECKHAND_REVISION}")" "$(promenade_render_validate_url)"
+    promenade_api_validatedesign "${VIA}" "${REF_URL}" "${ARGS}"
 
-    JOIN_CURL_URL="$(promenade_render_curl_url "${NAME}" "${USE_DECKHAND}" "${DECKHAND_REVISION}" "${LABELS[@]}")"
-    log "Fetching join script via: ${JOIN_CURL_URL}"
-    ssh_cmd "${VIA}" curl "${CURL_ARGS[@]}" \
-        "${JOIN_CURL_URL}" > "${SCRIPT_DIR}/join-${NAME}.sh"
+    log "Fetching join script"
+    JOIN_LABELS="$(render_labels "${LABELS[@]}")"
+    ARGS="--hostname "${NAME}" --ip "$(config_vm_ip "${NAME}")" --design-ref "${REF_URL}" "${JOIN_LABELS}" "${ARGS}""
+    promenade_api_join_scripts "${VIA}" "${ARGS}" "${NAME}"
 
-    chmod 755 "${SCRIPT_DIR}/join-${NAME}.sh"
     log "Join script received"
 
     log Joining node "${NAME}"
-    rsync_cmd "${SCRIPT_DIR}/join-${NAME}.sh" "${NAME}:/root/promenade/"
-    ssh_cmd "${NAME}" "/root/promenade/join-${NAME}.sh" 2>&1 | tee -a "${LOG_FILE}"
+    ssh_cmd "${VIA}" "/root/promenade/join-${NAME}.sh" 2>&1 | tee -a "${LOG_FILE}"
+    exit
 done
 
 sleep 10
